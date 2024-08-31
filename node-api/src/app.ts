43530149eb0b271c27fd8ec7633b8e839d1adecf
Developer Marcell <devmarcell@protonmail.com>
2024-08-31 15:28:02 -0300

    refactor: create start function

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -10,46 +10,61 @@ const app = fastify()
 
 app.register(fastifyCookie)
 
-app.post<{ Body: { username: string; password: string } }>(
-  '/login',
-  (req, reply) => {
-    const { username, password } = req.body
+async function startServer() {
+  try {
+    const db = Database.getInstance()
+    await db.getRepo()
 
-    if (username === env.username && password === env.password) {
-      const token = jwt.sign({ username }, env.jwtSecret, {
-        expiresIn: '30m'
-      })
+    app.register(fastifyCookie)
 
-      return reply
-        .setCookie('token', token, {
-          path: '/',
-          httpOnly: true,
-          secure: env.NODE_ENV === 'production',
-          maxAge: 30 * 60, // 30min
-          sameSite: 'strict'
-        })
-        .send({ message: 'Login successful' })
-    } else {
-      return reply.status(401).send({ message: 'Wrong username or password' })
-    }
-  }
-)
+    app.post<{ Body: { username: string; password: string } }>(
+      '/login',
+      (req, reply) => {
+        const { username, password } = req.body
 
-app.addHook('preHandler', auth)
+        if (username === env.username && password === env.password) {
+          const token = jwt.sign({ username }, env.jwtSecret, {
+            expiresIn: '30m'
+          })
 
-app.get('/data', async (_, reply) => {
-  const db = Database.getInstance()
-  const repo = await db.getRepo()
+          return reply
+            .setCookie('token', token, {
+              path: '/',
+              httpOnly: true,
+              secure: env.NODE_ENV === 'production',
+              maxAge: 30 * 60, // 30min
+              sameSite: 'strict'
+            })
+            .send({ message: 'Login successful' })
+        } else {
+          return reply
+            .status(401)
+            .send({ message: 'Wrong username or password' })
+        }
+      }
+    )
 
-  const docs = await repo.find()
+    app.addHook('preHandler', auth)
 
-  return reply.send(docs)
-})
+    app.get('/data', async (_, reply) => {
+      try {
+        const db = Database.getInstance()
+        const repo = db.getRepo()
 
-app.listen({ port: env.PORT }, (err) => {
-  if (err) {
-    console.error(err)
+        const docs = await repo.find()
+        return reply.send(docs)
+      } catch (err) {
+        console.error('Failed to fetch data:', err)
+        return reply.status(500).send({ error: 'Failed to fetch data' })
+      }
+    })
+
+    await app.listen({ port: env.PORT })
+    console.log(`Server listening on port ${env.PORT}`)
+  } catch (err) {
+    console.error('Server startup error:', err)
     process.exit(1)
   }
-  console.log(`Server listening on port ${env.PORT}`)
-})
+}
+
+startServer()
