df705b2bb0441ab230d9021eee302fd9cb4f7319
Developer Marcell <devmarcell@protonmail.com>
2024-08-31 15:48:30 -0300

    feat: add db name and collection name in env

--- node-api/src/config/env.ts
+++ node-api/src/config/env.ts
@@ -1,5 +1,7 @@
 export const env = {
   DB_URL: process.env.DB_URL || 'mongodb://root:example@mongo:27017/',
+  DB_NAME: process.env.DB_NAME || 'stats',
+  collectionName: process.env.COLLECTION_NAME || 'docs',
   PORT: Number(process.env.PORT) || 3000,
   jwtSecret: process.env.JWT_SECRET || 's3cr3t@3MDA',
   username: process.env.USERNAME || 'admin',
