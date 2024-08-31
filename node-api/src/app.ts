4c4448108d294030e30309dd2617b0c91a0a2d40
Developer Marcell <devmarcell@protonmail.com>
2024-08-31 15:07:53 -0300

    a

---
+++ node-api/src/app.ts
@@ -0,0 +1,55 @@
+import fastify from 'fastify'
+import jwt from 'jsonwebtoken'
+import fastifyCookie from 'fastify-cookie'
+
+import { Database } from './db'
+import { env } from './config/env'
+import { auth } from './middlewares/auth'
+
+const app = fastify()
+
+app.register(fastifyCookie)
+
+app.post<{ Body: { username: string; password: string } }>(
+  '/login',
+  (req, reply) => {
+    const { username, password } = req.body
+
+    if (username === env.username && password === env.password) {
+      const token = jwt.sign({ username }, env.jwtSecret, {
+        expiresIn: '30m'
+      })
+
+      return reply
+        .setCookie('token', token, {
+          path: '/',
+          httpOnly: true,
+          secure: env.NODE_ENV === 'production',
+          maxAge: 30 * 60, // 30min
+          sameSite: 'strict'
+        })
+        .send({ message: 'Login successful' })
+    } else {
+      return reply.status(401).send({ message: 'Wrong username or password' })
+    }
+  }
+)
+
+app.addHook('preHandler', auth)
+
+app.get('/data', async (_, reply) => {
+  const db = Database.getInstance()
+  const repo = await db.getRepo()
+
+  const docs = await repo.find()
+
+  return reply.send(docs)
+})
+
+app.listen({ port: env.PORT }, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening on port ${env.PORT}`)
+})
