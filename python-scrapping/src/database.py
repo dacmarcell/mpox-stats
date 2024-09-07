f7f97ad3165ee4958c0e741ade375a15f9a0ed23
Developer Marcell <devmarcell@protonmail.com>
2024-09-06 23:19:29 -0300

    refactor: use environment variables

--- python-scrapping/src/database.py
+++ python-scrapping/src/database.py
@@ -1,8 +1,16 @@
 from pymongo import MongoClient
+import os
 
 def save(text):
     try:
-        client = MongoClient("mongodb://root:example@mongo:27017/")
+        mongo_user = os.getenv("MONGO_USER")
+        mongo_password = os.getenv("MONGO_PASSWORD")
+        mongo_host = os.getenv("MONGO_HOST", "localhost")
+        mongo_port = os.getenv("MONGO_PORT", "27017")
+
+        mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/"
+        client = MongoClient(mongo_uri)
+        
         db = client["stats"]
         collection = db["docs"]
 
