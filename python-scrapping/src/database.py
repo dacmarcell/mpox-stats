168b91279acd8b4109df25b3d50d204c21985f14
Developer Marcell <devmarcell@protonmail.com>
2024-09-06 23:25:00 -0300

    fix: rename environment variable

--- python-scrapping/src/database.py
+++ python-scrapping/src/database.py
@@ -4,13 +4,14 @@ import os
 def save(text):
     try:
         mongo_user = os.getenv("MONGO_USER")
-        mongo_password = os.getenv("MONGO_PASSWORD")
+        mongo_pass = os.getenv("MONGO_PASS")
         mongo_host = os.getenv("MONGO_HOST", "localhost")
         mongo_port = os.getenv("MONGO_PORT", "27017")
 
-        mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/"
+        mongo_uri = f"mongodb://{mongo_user}:{mongo_pass}@{mongo_host}:{mongo_port}/"
+
         client = MongoClient(mongo_uri)
-        
+
         db = client["stats"]
         collection = db["docs"]
 
